943d0882b6756c3ba5e074107e3c1db71fee59fe
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-30 14:40:01 -0400

    database associations created

--- models/index.js
+++ models/index.js
@@ -4,3 +4,24 @@ const UserModel = require('./user');
 const PetModel = require('./pet');
 const RequestModel = require('./request');
 const ReviewModel = require('./review')
+
+
+module.exports = {
+    dbConnection: db,
+    UserModel,
+    PetModel,
+    RequestModel,
+    ReviewModel
+};
+
+UserModel.hasMany(PetModel);
+UserModel.hasMany(RequestModel);
+UserModel.hasMany(ReviewModel);
+
+PetModel.belongsTo(UserModel);
+PetModel.hasMany(ReviewModel);
+PetModel.hasMany(RequestModel);
+
+RequestModel.belongsTo(PetModel);
+
+ReviewModel.belongsTo(PetModel);
