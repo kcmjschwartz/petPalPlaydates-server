f6889cf3548335022c2214bc1471195c14cef92b
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-30 13:50:57 -0400

    pet, requests, and review model updated

--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
 const db = require('../db');
 
 const UserModel = require('./user');
+const PetModel = require('./pet');
+const RequestModel = require('./request');
+const ReviewModel = require('./review')
--- models/pet.js
+++ models/pet.js
@@ -2,7 +2,22 @@ const { DataTypes } = require('sequelize');
 const db = require("../db");
 
 const Pet = db.define("pet", {
-
+    petName:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    petType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
 });
 
 module.exports = Pet;
